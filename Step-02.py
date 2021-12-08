828b3f354de4150c6aaf7072cef9dcb1a678b0ce
Minki Song <chabliss23@naver.com>
2021-12-08 20:33:32 +0900

    프롬프트 반복 성공
    프롬프트 반복은 성공했지만 p가 입력되었을 때 프로그램 종료가 안됨.
    추가적으로 플레이어가 움직이기는 하는데 이상함.

--- Step-02.py
+++ Step-02.py
@@ -6,78 +6,102 @@ for i in range(len(stage2)):
     print(stage2[i])
 print()
 
-#문자열을 리스트로 변환 (문자열은 문자 하나의 값에 접근할 수는 있지만 변경할 수 없기 때문)
-for i in range(len(stage2)):
-    stage2[i] = list(stage2[i])
+#문자열을 리스트로 변환하는 함수 (문자열은 문자 하나의 값에 접근할 수는 있지만 변경할 수 없기 때문)
+def con():
+    for i in range(len(stage2)):
+        stage2[i] = list(stage2[i])
 
-#입력값 받기
-print("SOKOBAN>",end=" ")
-move = list(input())
+con()
 
-#플레이어 위치 찾기
-player = []     #player[0] => y좌표, player[1] => x좌표
 stage2_high = 7 #스테이지 세로크기
-
-for i in range(stage2_high):
-    for j in range(len(stage2[i])):
-        if stage2[i][j] == "P":
-            player.append(i)
-            player.append(j)
-            break
+player = []     #player[0] => y좌표, player[1] => x좌표
 
 #스테이지를 문자열로 바꿔서 출력하는 함수
-def pt():
+def pt():    #print줄임말
     for i in range(stage2_high):
         stage2[i] = "".join(stage2[i])
     for i in range(stage2_high):
         print(stage2[i])
+    print()    
+
+#플레이어 위치 찾는 함수
+def find():
+        
+    for i in range(stage2_high):
+        for j in range(len(stage2[i])):
+            if stage2[i][j] == "P":
+                player.append(i)
+                player.append(j)
+                break
+
+find()
+
+#입력값 받기(q받아서 프로그램 종료될까지)
+while True:
+    print("SOKOBAN>",end=" ")
+    move = list(input())
+    print()
 
 #입력값 실행하기
-for i in range(len(move)):
+    for i in range(len(move)):
 
-    if move[i] == "q":
-        print("Bye~")
-        break
+        con()
+        find()
 
-    elif move[i] == "w":
-        if stage2[player[0]-1][player[1]] == " ":
-            stage2[player[0]][player[1]] = " "
-            stage2[player[0]-1][player[1]] = "P"
-            print(move[i].upper(),": 위쪽으로 이동합니다.")
-            pt()
+        if move[i] == "q":
+            print("Bye~")
+            break
+
+        elif move[i] == "w":
+            if stage2[player[0]-1][player[1]] == " ":
+                stage2[player[0]][player[1]] = " "
+                stage2[player[0]-1][player[1]] = "P"
+                pt()
+                print(move[i].upper(),": 위쪽으로 이동합니다.")
+                
+                
+            else:
+                pt()
+                print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")   
+
+        elif move[i] == "a":
+            if stage2[player[0]][player[1]-1] == " ":
+                stage2[player[0]][player[1]] = " "
+                stage2[player[0]][player[1]-1] = "P"
+                pt()
+                print(move[i].upper(),": 왼쪽으로 이동합니다.")
+                
+                
+            else:
+                pt()
+                print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")               
+
+        elif move[i] == "s":
+            if stage2[player[0]+1][player[1]] == " ":
+                stage2[player[0]][player[1]] = " "
+                stage2[player[0]+1][player[1]] = "P"
+                pt()
+                print(move[i].upper(),": 아래쪽으로 이동합니다.")
+                
+                
+            else:
+                pt()
+                print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")    
+
+        elif move[i] == "d":
+            if stage2[player[0]][player[1]+1] == " ":
+                stage2[player[0]][player[1]] = " "
+                stage2[player[0]][player[1]+1] = "P"
+                pt()
+                print(move[i].upper(),": 오른쪽으로 이동합니다.")
+                
+                
+            else:
+                pt()
+                print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")
+        
         else:
             print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")
-            pt()
-
-    elif move[i] == "a":
-        if stage2[player[0]][player[1]-1] == " ":
-            stage2[player[0]][player[1]] = " "
-            stage2[player[0]][player[1]-1] = "P"
-            print(move[i].upper(),": 왼쪽으로 이동합니다.")
-            pt()
-        else:
-            print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")
-            pt()
-
-    elif move[i] == "s":
-        if stage2[player[0]+1][player[1]] == " ":
-            stage2[player[0]][player[1]] = " "
-            stage2[player[0]+1][player[1]] = "P"
-            print(move[i].upper(),": 아래쪽으로 이동합니다.")
-            pt()
-        else:
-            print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")
-            pt()
-
-    elif move[i] == "d":
-        if stage2[player[0]][player[1]+1] == " ":
-            stage2[player[0]][player[1]] = " "
-            stage2[player[0]][player[1]+1] = "P"
-            print(move[i].upper(),": 오른쪽으로 이동합니다.")
-            pt()
-        else:
-            print(move[i].upper(),": (경고!) 해당 명령을 수행할 수 없습니다!")
-            pt()
     
 
 
